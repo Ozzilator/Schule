1f2032c37a30457250da99297d2f2ed632d389d8
Ozzilator <Niggl.Keepo@web.de>
2016-03-01 15:33:05 +0100

    working
    while

--- CLR_Strings_lange/CLR_Strings_lange/CLR_Strings_lange.cpp
+++ CLR_Strings_lange/CLR_Strings_lange/CLR_Strings_lange.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 string String(string);
 string MAC(string, int);
-string GET_IP_zehn(string);
+string MAC_IP_zehn(string);
+string PC_Name(string);
+
 int main()
 {
 	string str;
@@ -17,18 +19,21 @@ int main()
 	int erstes = str.find(",");
 	int zweites = str.find(",", erstes + 1);
 	int IP_mit_zehn = str.find("10.");
-	int MAC_mit_Minus = str.find("MAC: ");
+	int MAC_mit_Minus = str.find("IP: 10") + 15;
+	int PC_Name = str.find("cn=");
 
 	cout << "Bearbeitung von folgendem String: \n\n";
 	cout << str << endl << endl;
-
 	cout << "erstes Komma: \t\t\t" << erstes << endl;
 	cout << "zweites Komma: \t\t\t" << zweites << endl;
 	cout << "IP-Nummer beginnend mit 10:\t" << IP_mit_zehn << endl;
 	cout << "MAC fuer IP mit 10:\t\t" << MAC_mit_Minus << endl;
 
-	system("Pause");
-	GET_IP_zehn(str);
+	cout << "Der PC hat den Namen:\t\t\t" << PC_Name << endl;
+
+	system("pause");
+	PC_Name(str);
+	MAC_IP_zehn(str);
 	system("pause");
 	return 0;
 }
@@ -45,22 +50,19 @@ string MAC(string str, int MAC_mit_Minus)
 	str.erase(str.find(";"), (str.length() - str.find("MAC: ")));
 	return str;
 }
-string GET_IP_zehn(string str)
+string MAC_IP_zehn(string str)
 {
-	/*
-	int IP_mit_zehn = str.find("10.");
-	int kleinster, groeßter;
-	kleinster = str.find("MAC: ");
-	groeßter = str.find("MAC :", kleinster);
-
-	if ((IP_mit_zehn - groeßter) < kleinster)
-	{
-
-	}
-	*/
-	int MAC_mit_Minus = str.find("MAC: ");
-	cout << "#####" << MAC_mit_Minus << endl;
-	MAC(str, MAC_mit_Minus);
-	cout << str.length() << endl << str;
+	int ip_zehn = str.find("IP: 10");
+	str.erase(ip_zehn, 15);
+	str.erase(0, ip_zehn);
+	str.end();
+	str.erase(str.find("GUID: ")-1, 50);
+	return str;
+}
+string PC_Name(string str)
+{
+	int CN = str.find("cn=");
+	str.erase(CN, str.length() - str.find(","));
+	cout << str << endl;
 	return str;
 }
